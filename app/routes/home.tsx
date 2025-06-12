d69fc59ace2c32a081fb71fbe032fd72b95d0388
ksuke0322 <ksuke.0322@gmail.com>
2025-06-12 22:54:58 +0900

    chore: add dev settings

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ]
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const clientLoader = async (): Promise<any[]> => {
+  const response = await fetch('https://endoflife.date/api/nodejs.json')
+  const data = await response.json()
+  console.log(data)
+
+  return data
 }
 
-export default function Home() {
-  return <Welcome />;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Home({ loaderData }: { loaderData: any[] }) {
+  return (
+    <div>
+      {/* <Welcome /> */}
+      <p>{JSON.stringify(loaderData, null, 2)}</p>
+    </div>
+  )
 }
---
+++ eslint.config.mjs
@@ -0,0 +1,180 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+import unusedImports from 'eslint-plugin-unused-imports'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
+import jsxA11Y from 'eslint-plugin-jsx-a11y'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import _import from 'eslint-plugin-import'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: ['.husky/**/*'],
+  },
+  ...fixupConfigRules(
+    compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  ),
+  ...fixupConfigRules(
+    compat.extends(
+      'plugin:react-hooks/recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+      'plugin:jsx-a11y/recommended',
+    ),
+  ).map((config) => ({
+    ...config,
+    files: ['**/*.{ts,tsx}'],
+  })),
+  {
+    ignores: [
+      'eslint.config.mjs',
+      'postcss.config.cjs',
+      'react-router.config.ts',
+      'tailwind.config.ts',
+      'vite.config.ts',
+      'build/**/*',
+      '.react-router/**/*',
+    ],
+
+    plugins: {
+      'unused-imports': unusedImports,
+      'react-refresh': reactRefresh,
+      react: fixupPluginRules(react),
+      'jsx-a11y': fixupPluginRules(jsxA11Y),
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+    ),
+  ).map((config) => ({
+    ...config,
+    files: ['**/*.{ts,tsx}'],
+  })),
+  {
+    files: ['**/*.{ts,tsx}'],
+
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
+      'unused-imports': unusedImports,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/app.css',
+      },
+
+      'import/internal-regex': '^~/',
+
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx', '.svg'],
+        },
+
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+
+      react: {
+        version: 'detect',
+      },
+
+      formComponents: ['Form'],
+
+      linkComponents: [
+        {
+          name: 'Link',
+          linkAttribute: 'to',
+        },
+        {
+          name: 'NavLink',
+          linkAttribute: 'to',
+        },
+      ],
+    },
+
+    rules: {
+      'unused-imports/no-unused-imports': 'warn',
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+
+          alphabetize: {
+            order: 'asc',
+          },
+
+          'newlines-between': 'always',
+        },
+      ],
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+
+      'react/prop-types': 0,
+
+      // 'react-refresh/only-export-components': [
+      //   'warn',
+      //   {
+      //     allowConstantExport: true,
+      //   },
+      // ],
+    },
+  },
+]
